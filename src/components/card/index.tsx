e8dca40f8b29f77345fdb3ae9297d4ffe11c1a1f
manav <3251manavmattu@gmail.com>
2024-10-07 15:52:52 +0530

    .

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,47 +1,50 @@
-// import React from "react";
-import "../card/index.css";
-// import { Card, CardContent, Typography, Button } from "@mui/material";
-import {  cardDataType1 , cardDataType2 } from "../../data/cardData";
+import { cardDataType1, cardDataType2 } from "../../data/cardData";
 
-const MyCard = ({type}) => {
+const MyCard = ({ type }) => {
+  const cardData = type === "type1" ? cardDataType1 : cardDataType2;
 
-    const cardData = type === "type1" ? cardDataType1 : cardDataType2;
-
-    return (
-      <div className="cards-wrapper">
-        {cardData.map((card, index) => (
-
-            
-          <div className={`card-container ${type === "type1" ? "type1" : "type2"}`} key={index}>
-            <a className="card-image-wrapper" href={card.link}>
-              <figure className="card-image">
-                <img alt={card.alt} src={card.imgSrc} />
-              </figure>
-            </a>
-            <div className="card-content">
-              <h2 className="card-title">{card.title}</h2>
-              <div className="card-description">
-                <div>{card.description}</div>
-              </div>
-              <div className="card-button-wrapper">
-                <a href={card.buttonLink} className="card-button">
-                  <span>{card.buttonText}</span>
-                  <span className="card-button-icon">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 32 32"
-                      fill="currentColor"
-                    >
-                      <path d="M 21.1875 9.28125 L 19.78125 10.71875 L 24.0625 15 L 4 15 L 4 17 L 24.0625 17 L 19.78125 21.28125 L 21.1875 22.71875 L 27.90625 16 Z"></path>
-                    </svg>
-                  </span>
-                </a>
-              </div>
+  return (
+    <div className="flex flex-wrap justify-center gap-5 p-5">
+      {cardData.map((card, index) => (
+        <div
+          key={index}
+          className={`w-[400px] text-center bg-white border border-gray-300 rounded-lg p-6 shadow-md ${
+            type === "type1" ? "bg-white text-black" : "bg-[#f1f1ff] text-black"
+          }`}
+        >
+          <a className="flex justify-center items-center mb-5" href={card.link}>
+            <figure className="w-12 m-0">
+              <img alt={card.alt} src={card.imgSrc} className="w-full h-auto" />
+            </figure>
+          </a>
+          <div className="text-center">
+            <h2 className="text-lg font-bold mb-3">{card.title}</h2>
+            <div className="mb-5">
+              <p>{card.description}</p>
+            </div>
+            <div className="flex justify-center items-center">
+              <a
+                href={card.buttonLink}
+                className="bg-black text-white py-2 px-6 rounded-full flex items-center justify-center transition-colors duration-300 hover:bg-gray-800"
+              >
+                <span>{card.buttonText}</span>
+                <span className="ml-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 32 32"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path d="M 21.1875 9.28125 L 19.78125 10.71875 L 24.0625 15 L 4 15 L 4 17 L 24.0625 17 L 19.78125 21.28125 L 21.1875 22.71875 L 27.90625 16 Z"></path>
+                  </svg>
+                </span>
+              </a>
             </div>
           </div>
-        ))}
-      </div>
-    );
-  };
+        </div>
+      ))}
+    </div>
+  );
+};
 
-  export default MyCard;
+export default MyCard;
